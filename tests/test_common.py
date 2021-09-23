f2230f44918446a4ad0904833972eae9f6733273
yaosheng <yaosheng@e-lead.cn>
2021-09-23 16:47:07 +0800

    commit code

---
+++ tests/test_common.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+
+# -------------------------------------------------------------------------
+# Author : Yifeng-Chen
+# -------------------------------------------------------------------------
+
+import unittest
+
+class Simple_Test(unittest.TestCase):
+
+    def test_sample(self):
+        self.assertTrue(True)
+
+if __name__ == "__main__":
+    unittest.main()
