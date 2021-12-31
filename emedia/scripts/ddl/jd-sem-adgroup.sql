f3da1d382fc754c2b7d9175af8fa780dd3ebf40d
yifeng_chen <yifeng_chen@epam.com>
2021-12-31 16:16:48 +0800

    jd_sem_adgroup ddl

---
+++ emedia/scripts/ddl/jd-sem-adgroup.sql
@@ -0,0 +1,132 @@
+-- jd sem adgroup
+DROP TABLE IF EXISTS `dws`.`tb_emedia_jd_sem_adgroup_mapping_success`;
+CREATE TABLE IF NOT EXISTS `dws`.`tb_emedia_jd_sem_adgroup_mapping_success` (
+  campaignId STRING
+  , date STRING
+  , mobileType STRING
+  , campaignType STRING
+  , adGroupId STRING
+  , adGroupName STRING
+  , deliveryVersion STRING
+  , campaignName STRING
+  , pin STRING
+  , req_startDay STRING
+  , req_endDay STRING
+  , req_productName STRING
+  , req_clickOrOrderDay STRING
+  , req_clickOrOrderCaliber STRING
+  , req_orderStatusCategory STRING
+  , req_giftFlag STRING
+  , req_isDaily STRING
+  , req_page STRING
+  , req_pageSize STRING
+  , req_pin STRING
+  , source STRING
+  , directOrderSum STRING
+  , shopAttentionCnt STRING
+  , totalOrderSum STRING
+  , directCartCnt STRING
+  , CPC STRING
+  , totalOrderROI STRING
+  , totalOrderCVS STRING
+  , clicks STRING
+  , CPA STRING
+  , CTR STRING
+  , CPM STRING
+  , impressions STRING
+  , couponCnt STRING
+  , indirectOrderSum STRING
+  , visitorCnt STRING
+  , totalPresaleOrderCnt STRING
+  , presaleDirectOrderCnt STRING
+  , cost STRING
+  , visitPageCnt STRING
+  , goodsAttentionCnt STRING
+  , presaleDirectOrderSum STRING
+  , directOrderCnt STRING
+  , presaleIndirectOrderSum STRING
+  , indirectCartCnt STRING
+  , visitTimeAverage STRING
+  , presaleIndirectOrderCnt STRING
+  , indirectOrderCnt STRING
+  , newCustomersCnt STRING
+  , depthPassengerCnt STRING
+  , totalCartCnt STRING
+  , totalPresaleOrderSum STRING
+  , preorderCnt STRING
+  , totalOrderCnt STRING
+  , category_id STRING
+  , brand_id STRING
+  , `etl_date` DATE
+  , `etl_create_time` TIMESTAMP
+)
+USING delta
+PARTITIONED BY (`date`)
+LOCATION  'dbfs:/mnt/${spark.dbr_env}/data_warehouse/media_dws.db/tb_emedia_jd_sem_adgroup_mapping_success';
+
+
+DROP TABLE IF EXISTS `stg`.`tb_emedia_jd_sem_adgroup_mapping_fail`;
+CREATE TABLE IF NOT EXISTS `stg`.`tb_emedia_jd_sem_adgroup_mapping_fail` (
+  campaignId STRING
+  , date STRING
+  , mobileType STRING
+  , campaignType STRING
+  , adGroupId STRING
+  , adGroupName STRING
+  , deliveryVersion STRING
+  , campaignName STRING
+  , pin STRING
+  , req_startDay STRING
+  , req_endDay STRING
+  , req_productName STRING
+  , req_clickOrOrderDay STRING
+  , req_clickOrOrderCaliber STRING
+  , req_orderStatusCategory STRING
+  , req_giftFlag STRING
+  , req_isDaily STRING
+  , req_page STRING
+  , req_pageSize STRING
+  , req_pin STRING
+  , source STRING
+  , directOrderSum STRING
+  , shopAttentionCnt STRING
+  , totalOrderSum STRING
+  , directCartCnt STRING
+  , CPC STRING
+  , totalOrderROI STRING
+  , totalOrderCVS STRING
+  , clicks STRING
+  , CPA STRING
+  , CTR STRING
+  , CPM STRING
+  , impressions STRING
+  , couponCnt STRING
+  , indirectOrderSum STRING
+  , visitorCnt STRING
+  , totalPresaleOrderCnt STRING
+  , presaleDirectOrderCnt STRING
+  , cost STRING
+  , visitPageCnt STRING
+  , goodsAttentionCnt STRING
+  , presaleDirectOrderSum STRING
+  , directOrderCnt STRING
+  , presaleIndirectOrderSum STRING
+  , indirectCartCnt STRING
+  , visitTimeAverage STRING
+  , presaleIndirectOrderCnt STRING
+  , indirectOrderCnt STRING
+  , newCustomersCnt STRING
+  , depthPassengerCnt STRING
+  , totalCartCnt STRING
+  , totalPresaleOrderSum STRING
+  , preorderCnt STRING
+  , totalOrderCnt STRING
+  , category_id STRING
+  , brand_id STRING
+  , `etl_date` DATE
+  , `etl_create_time` TIMESTAMP
+)
+USING delta
+PARTITIONED BY (`date`)
+LOCATION  'dbfs:/mnt/${spark.dbr_env}/data_warehouse/media_stg.db/tb_emedia_jd_sem_adgroup_mapping_fail';
+
