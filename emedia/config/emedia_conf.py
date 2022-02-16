f4919d88d9070d10789f56ebfd950439a673cb2a
yaosheng <yaosheng@e-lead.cn>
2022-02-16 15:59:48 +0800

    update read emedia_conf_dict

--- emedia/config/emedia_conf.py
+++ emedia/config/emedia_conf.py
@@ -12,51 +12,29 @@ dbutils = get_dbutils()
 
 
 def get_emedia_conf_dict():
-    dev = {
-        'input_blob_account': dbutils.secrets.get("qa-media-scope", "input-blob-account")
-        , 'input_blob_container': dbutils.secrets.get("qa-media-scope", "input-blob-container")
-        , 'input_blob_sas': dbutils.secrets.get("qa-media-scope", "input-blob-sas")
-
-        , 'mapping_blob_account': dbutils.secrets.get("qa-media-scope", "target-blob-account")
-        , 'mapping_blob_container': dbutils.secrets.get("qa-media-scope", "target-blob-container")
-        , 'mapping_blob_sas': dbutils.secrets.get("qa-media-scope", "target-blob-sas")
-
-        , 'target_blob_account': dbutils.secrets.get("qa-media-scope", "target-blob-account")
-        , 'target_blob_container': dbutils.secrets.get("qa-media-scope", "target-blob-container")
-        , 'target_blob_sas': dbutils.secrets.get("qa-media-scope", "target-blob-sas")
-
-        , 'emedia_input_scope': ''
-        , 'emedia_input_key': ''
-
-        , 'emedia_output_scope': ''
-        , 'emedia_output_key': ''
-    }
-
-    qa = dev
-
-    prod = {
-         'input_blob_account': dbutils.secrets.get("prd-media-scope", "input-blob-account")
-        , 'input_blob_container': dbutils.secrets.get("prd-media-scope", "input-blob-container")
-        , 'input_blob_sas': dbutils.secrets.get("prd-media-scope", "input-blob-sas")
-
-        , 'mapping_blob_account': dbutils.secrets.get("prd-media-scope", "target-blob-account")
-        , 'mapping_blob_container': dbutils.secrets.get("prd-media-scope", "target-blob-container")
-        , 'mapping_blob_sas': dbutils.secrets.get("prd-media-scope", "target-blob-sas")
-
-        , 'target_blob_account': dbutils.secrets.get("prd-media-scope", "target-blob-account")
-        , 'target_blob_container': dbutils.secrets.get("prd-media-scope", "target-blob-container")
-        , 'target_blob_sas': dbutils.secrets.get("prd-media-scope", "target-blob-sas")
-
-    }
-
     if not os.path.exists('/dbfs/mnt/databricks_conf.ini'):
         return 'qa'
     dbfsCfgParser = configparser.ConfigParser()
     dbfsCfgParser.read('/dbfs/mnt/databricks_conf.ini')
+    env = "qa"
+    if dbfsCfgParser['common']['env'] != 'qa':
+        env = "prd"
+    scope_name = env+"_media-scope"
+    scope_conf_dict = {
+         'input_blob_account': dbutils.secrets.get(scope_name, "input-blob-account")
+        , 'input_blob_container': dbutils.secrets.get(scope_name, "input-blob-container")
+        , 'input_blob_sas': dbutils.secrets.get(scope_name, "input-blob-sas")
 
-    if dbfsCfgParser['common']['env'] == 'qa':
-        return qa
-    else:
-        return prod
+        , 'mapping_blob_account': dbutils.secrets.get(scope_name, "target-blob-account")
+        , 'mapping_blob_container': dbutils.secrets.get(scope_name, "target-blob-container")
+        , 'mapping_blob_sas': dbutils.secrets.get(scope_name, "target-blob-sas")
+
+        , 'target_blob_account': dbutils.secrets.get(scope_name, "target-blob-account")
+        , 'target_blob_container': dbutils.secrets.get(scope_name, "target-blob-container")
+        , 'target_blob_sas': dbutils.secrets.get(scope_name, "target-blob-sas")
+
+    }
+    return scope_conf_dict
+
 
 
