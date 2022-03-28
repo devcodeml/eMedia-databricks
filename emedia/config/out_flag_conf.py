9a6207e14558dbd69cbf3acb0efd6c1d3f05ba6f
liuyaxu <361448318@qq.com>
2022-03-28 10:48:22 +0800

    fix:fix GM out_flag blob

---
+++ emedia/config/out_flag_conf.py
@@ -0,0 +1,3 @@
+def get_out_flag_blob_prefix():
+    out_flag_blob_prefix_list = list('target', 'cumul')
+    return out_flag_blob_prefix_list
--- emedia/utils/output_df.py
+++ emedia/utils/output_df.py
@@ -7,23 +7,25 @@ from emedia import spark
 import random, string
 
 from emedia.config.emedia_conf import get_emedia_conf_dict
+from emedia.config.out_flag_conf import get_out_flag_blob_prefix
 
 
 def _rename_blob_file(prefix, new_file_name, dest_account, dest_container, dest_sas):
-    
-    blob_service = BlockBlobService(account_name=dest_account, sas_token=dest_sas, endpoint_suffix='core.chinacloudapi.cn')
+    blob_service = BlockBlobService(account_name=dest_account, sas_token=dest_sas,
+                                    endpoint_suffix='core.chinacloudapi.cn')
 
     blob_list = blob_service.list_blobs(dest_container, prefix=prefix)
 
     for blob in blob_list:
         if (blob.name.find("part") != -1):
             copy_from_container = dest_container
-            blob_url = blob_service.make_blob_url(container_name=dest_container, blob_name=blob.name, sas_token=dest_sas)
+            blob_url = blob_service.make_blob_url(container_name=dest_container, blob_name=blob.name,
+                                                  sas_token=dest_sas)
             blob_service.copy_blob(dest_container, new_file_name, blob_url)
             blob_service.delete_blob(copy_from_container, blob.name)
 
 
-def output_to_emedia(df, parent_path, filename,sep=r'\\001',dict_key='target',**option):
+def output_to_emedia(df, parent_path, filename, sep=r'\\001', dict_key='target', **option):
     '''
     Output dataframe as one CSV file
 
@@ -45,16 +47,16 @@ def output_to_emedia(df, parent_path, filename,sep=r'\\001',dict_key='target',**
     tmp_path = f"tmp/{random_str}/{parent_path}".replace(':', '_', -1)
 
     df.coalesce(1).write.csv(
-        path = f"wasbs://{container}@{account}.blob.core.chinacloudapi.cn/{tmp_path}"
-        , header = True
-        , sep = sep
-        , mode = "overwrite"
-        , quote = "\""
-        , nullValue = u'\u0000'
-        , emptyValue = u'\u0000'
-        ,**option
+        path=f"wasbs://{container}@{account}.blob.core.chinacloudapi.cn/{tmp_path}"
+        , header=True
+        , sep=sep
+        , mode="overwrite"
+        , quote="\""
+        , nullValue=u'\u0000'
+        , emptyValue=u'\u0000'
+        , **option
     )
-    
+
     data_writer.delete_blob_mark_file(tmp_path, container, account, sas, 'core.chinacloudapi.cn')
 
     _rename_blob_file(tmp_path, f"{parent_path}/{filename}", account, container, sas)
@@ -64,27 +66,31 @@ def output_to_emedia(df, parent_path, filename,sep=r'\\001',dict_key='target',**
 
 def create_blob_by_text(full_file_path, text):
     emedia_conf_dict = get_emedia_conf_dict()
-    account = emedia_conf_dict.get('target_blob_account')
-    container = emedia_conf_dict.get('target_blob_container')
-    sas = emedia_conf_dict.get('target_blob_sas')
+    flag_blob_list = get_out_flag_blob_prefix()
 
-    blob_service = BlockBlobService(
-                    account_name = account
-                    , sas_token = sas
-                    , endpoint_suffix = 'core.chinacloudapi.cn'
-    )
-    
-    blob_service.create_blob_from_text(
-                    container
-                    , full_file_path
-                    , text
-                    , encoding = 'utf-8'
-    )
+    if flag_blob_list:
+        for prefix in flag_blob_list:
+            account = emedia_conf_dict.get(f'{prefix}' + '_blob_account')
+            container = emedia_conf_dict.get(f'{prefix}' + '_blob_container')
+            sas = emedia_conf_dict.get(f'{prefix}' + '_blob_sas')
+
+            blob_service = BlockBlobService(
+                account_name=account
+                , sas_token=sas
+                , endpoint_suffix='core.chinacloudapi.cn'
+            )
+
+            blob_service.create_blob_from_text(
+                container
+                , full_file_path
+                , text
+                , encoding='utf-8'
+            )
 
     return 0
 
-def write_eab_db(df,run_id,table_name):
 
+def write_eab_db(df, run_id, table_name):
     emedia_conf_dict = get_emedia_conf_dict()
     url = emedia_conf_dict["mysql_url"]
     user = emedia_conf_dict["mysql_user"]
@@ -92,8 +98,11 @@ def write_eab_db(df,run_id,table_name):
 
     size = df.count()
 
-    data_writer.write_to_db(df=df, url=url, mode="append", driver='com.mysql.jdbc.Driver', dbtable=table_name,user=user, password=password)
+    data_writer.write_to_db(df=df, url=url, mode="append", driver='com.mysql.jdbc.Driver', dbtable=table_name,
+                            user=user, password=password)
 
     # log insert
-    log_df = spark.sql(f"""select '{run_id}' as batchid,'{table_name}' as table_name,now() as created_time,{size} as size""")
-    data_writer.write_to_db(df=log_df, url=url, mode="append", driver='com.mysql.jdbc.Driver', dbtable="eab_log",user=user, password=password)
+    log_df = spark.sql(
+        f"""select '{run_id}' as batchid,'{table_name}' as table_name,now() as created_time,{size} as size""")
+    data_writer.write_to_db(df=log_df, url=url, mode="append", driver='com.mysql.jdbc.Driver', dbtable="eab_log",
+                            user=user, password=password)
