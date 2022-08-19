7c1feaa226fb12bc13b6d138d679ab8f508d7bd0
song-yw-1 <songyawei@mininglamp.com>
2022-08-19 16:46:20 +0800

    新增万象台数据处理

--- emedia/processing/tmall/tmail_wxt_day_campaign.py
+++ emedia/processing/tmall/tmail_wxt_day_campaign.py
@@ -7,7 +7,7 @@ from pyspark.sql.functions import current_date, current_timestamp
 from emedia.config.emedia_conf import get_emedia_conf_dict
 from emedia.utils.output_df import output_to_emedia, create_blob_by_text
 from pyspark.sql import functions as F
-
+from pyspark.sql.types import *
 def tamll_wxt__day_campaign_etl(airflow_execution_date):
 
     etl_year = int(airflow_execution_date[0:4])
@@ -53,25 +53,59 @@ def tamll_wxt__day_campaign_etl(airflow_execution_date):
  'campaign_id','`req_report_query.campaign_name` as campaign_name', '`req_api_service_context.biz_code` as biz_code', 'item_id', 'charge', 'click', 'ad_pv', 'ctr', 'ecpm','ecpc', 'car_num', 'dir_car_num', 'indir_car_num',
  'inshop_item_col_num', 'inshop_item_col_car_num_cost', 'alipay_inshop_amt', 'alipay_inshop_num', 'cvr', 'roi', 'prepay_inshop_amt', 'prepay_inshop_num', 'no_lalipay_inshop_amt_proprtion', 'dir_alipay_inshop_num', 'dir_alipay_inshop_amt', 'indir_alipay_inshop_num', 'indir_alipay_inshop_amt', 'sample_alipay_num', 'sample_alipay_amt',"'taobao.onebp.dkx.report.report.campaign.daylist' as data_source"
 )
+    report_df = report_df.withColumn('ecpm', report_df.ecpm.cast(DoubleType()))
     # spark.sql("drop table if exists stg.media_emedia_tmall_wxt_campaign_report")
     report_df.distinct().write.mode("overwrite").insertInto("stg.media_emedia_tmall_wxt_campaign_report")
 
-
-    daily_reports = report_df
+    fail_table_exist = spark.sql(
+        "show tables in dwd like 'media_emedia_tmall_wxt_campaign_report_fail'").count()
+    if fail_table_exist == 0:
+        daily_reports = report_df
+    else:
+        fail_df = spark.table("dwd.media_emedia_tmall_wxt_campaign_report_fail") \
+            .drop('category_id') \
+            .drop('brand_id') \
+            .drop('etl_date') \
+            .drop('etl_create_time')
+        daily_reports = report_df.union(fail_df)
 
     ad_type = 'wxt'
 
     ## 引用mapping函数 路径不一样自行修改函数路径
     res = emedia_brand_mapping(spark, daily_reports, ad_type)
-    # spark.sql("drop table if exists dwd.media_emedia_tmall_wxt_campaign_report_success")
-    res[0].distinct().write.mode("overwrite").insertInto("dwd.media_emedia_tmall_wxt_campaign_report_success")
+
+    res[0].createOrReplaceTempView("all_mapping_success")
+    table_exist = spark.sql(
+        "show tables in dwd like 'media_emedia_tmall_wxt_campaign_report_success'").count()
+    # AND dws.media_emedia_tmall_ylmf_day_campaignGroup_mapping_success.effect_days = all_mappint_success.effect_days
+    if table_exist == 0:
+        res[0].write.mode("overwrite").insertInto(
+            "dwd.media_emedia_tmall_wxt_campaign_report_success")
+    else:
+        spark.sql("""
+                  MERGE INTO dwd.media_emedia_tmall_wxt_campaign_report_success
+                  USING all_mapping_success
+                  ON dwd.media_emedia_tmall_wxt_campaign_report_success.store_id = all_mapping_success.store_id
+                      AND dwd.media_emedia_tmall_wxt_campaign_report_success.category_id = all_mapping_success.category_id
+                      AND dwd.media_emedia_tmall_wxt_campaign_report_success.brand_id = all_mapping_success.brand_id
+                      AND dwd.media_emedia_tmall_wxt_campaign_report_success.ad_date = all_mapping_success.ad_date
+                      AND dwd.media_emedia_tmall_wxt_campaign_report_success.effect = all_mapping_success.effect
+                      AND dwd.media_emedia_tmall_wxt_campaign_report_success.effect_type = all_mapping_success.effect_type
+                      AND dwd.media_emedia_tmall_wxt_campaign_report_success.campaign_id = all_mapping_success.campaign_id
+                      AND dwd.media_emedia_tmall_wxt_campaign_report_success.biz_code = all_mapping_success.biz_code
+                  WHEN MATCHED THEN
+                      UPDATE SET *
+                  WHEN NOT MATCHED
+                      THEN INSERT *
+                """)
+
     # spark.sql("drop table if exists dwd.media_emedia_tmall_wxt_campaign_report_fail")
     res[1].distinct().write.mode("overwrite").insertInto("dwd.media_emedia_tmall_wxt_campaign_report_fail")
 
 
     # 输出函数，你们需要自测一下
-    output_to_emedia(res[0].union(res[1]).drop('etl_date').drop('etl_create_time'), f'{date}/{date_time}/wxt',
-                     'EMEDIA_TMALL_WXT_DAILY_CAMPAIGN_REPORT_FACT')
+    output_to_emedia(spark.table("dwd.media_emedia_tmall_wxt_campaign_report_success").union(res[1]).drop('etl_date').drop('etl_create_time'), f'{date}/{date_time}/wxt',
+                     'EMEDIA_TMALL_WXT_DAILY_CAMPAIGN_REPORT_FACT.CSV')
 
     # create_blob_by_text(f"{date}/flag.txt", date_time)
 
@@ -170,7 +204,7 @@ def emedia_brand_mapping(spark,daily_reports,ad_type):
                 , m2.category_id
                 , m2.brand_id
             FROM mappint_fail_1 mfr1 LEFT JOIN mapping2 m2 ON mfr1.{0} = m2.account_id
-            AND instr(mfr1.{1}, m2.keyword) > 0
+            AND instr(upper(mfr1.{1}), upper(m2.keyword)) > 0
         '''.format(account_id,keyword))
     mappint2_result_df \
         .filter("category_id IS null and brand_id IS null") \
@@ -187,7 +221,7 @@ def emedia_brand_mapping(spark,daily_reports,ad_type):
                 , m3.category_id
                 , m3.brand_id
             FROM mappint_fail_2 mfr2 LEFT JOIN mapping3 m3 ON mfr2.{0} = m3.account_id
-            AND instr(mfr2.{1}, m3.keyword) > 0
+            AND instr(upper(mfr2.{1}), upper(m3.keyword)) > 0
         '''.format(account_id,keyword))
     mappint3_result_df \
         .filter("category_id is null and brand_id is null") \
