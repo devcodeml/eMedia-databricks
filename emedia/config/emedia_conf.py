25f96113369711b2ac4a991dc18b462a51ea949f
songyawei <song.yw.1@pg.com>
2022-10-12 19:20:02 +0800

    修改airflow时间

--- emedia/config/emedia_conf.py
+++ emedia/config/emedia_conf.py
@@ -47,7 +47,11 @@ def get_emedia_conf_dict():
         ,'dwwriteuser': 'etl_user_write'
         ,'dwwritepassword': '2wsXcde#'
         ,"dwurl" : "jdbc:sqlserver://b2bmptbiqa0101.database.chinacloudapi.cn:1433;database=B2B-qa-MPT-DW-01;encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.chinacloudapi.cn;loginTimeout=30;"
-
+        , 'synapsedwuser': dbutils.secrets.get("databrick-secret-scope", "synapsedwuser")
+        , 'synapsedwpw': dbutils.secrets.get("databrick-secret-scope", "synapsedwpw")
+        , 'synapseaccountname': dbutils.secrets.get("databrick-secret-scope", "synapseaccountname")
+        , 'synapsedirpath': dbutils.secrets.get("databrick-secret-scope", "synapsedirpath")
+        , 'synapsekey': dbutils.secrets.get("databrick-secret-scope", "synapsekey")
 
         , 'mysql_user': 'datalake@consumer-qa-emedia-db-0'
         , 'mysql_pwd': 'b2062ff9122811e99ce54f08370059c1'
