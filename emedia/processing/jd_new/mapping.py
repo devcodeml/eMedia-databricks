912d8ff254fb744750afe0baabff578a12156c01
songyawei <song.yw.1@pg.com>
2022-10-13 12:35:42 +0800

    更新伪代码

---
+++ emedia/processing/jd_new/mapping.py
@@ -0,0 +1,34 @@
+
+
+
+import datetime
+from pyspark.sql.functions import current_date, current_timestamp
+import pyspark.sql.functions as F
+
+from emedia import log
+
+
+def mapping_input(spark):
+
+    emedia_conf_dict = get_emedia_conf_dict()
+    mapping_blob_account = emedia_conf_dict.get('mapping_account')
+    mapping_blob_container = emedia_conf_dict.get('mapping_container')
+    mapping_blob_sas = emedia_conf_dict.get('mapping_sas')
+    # spark.conf.set(f"fs.azure.sas.{mapping_blob_container}.{mapping_blob_account}.blob.core.chinacloudapi.cn"
+    #                , mapping_blob_sas)
+    # mapping_blob_account = 'b2bmptbiprd01'
+    # mapping_blob_container = 'emedia-resource'
+    # mapping_blob_sas = 'st=2020-07-14T09%3A08%3A06Z&se=2030-12-31T09%3A08%3A00Z&sp=racwl&sv=2018-03-28&sr=c&sig=0YVHwfcoCDh53MESP2JzAD7stj5RFmFEmJbi5KGjB2c%3D'
+    spark.conf.set(f"fs.azure.sas.{mapping_blob_container}.{mapping_blob_account}.blob.core.chinacloudapi.cn", mapping_blob_sas)
+
+    emedia_sem_audience_mapping_path = 'emedia_sem_audience_mapping/emedia_sem_audience_mapping.csv'
+    log.info(f'emedia_sem_audience_mapping file: {emedia_sem_audience_mapping_path}')
+    emedia_sem_audience_mapping_df = spark.read.csv(
+        f"wasbs://{mapping_blob_container}@{mapping_blob_account}.blob.core.chinacloudapi.cn/{emedia_sem_audience_mapping_path}"
+        , header=True
+        , multiLine=True
+        , sep=","
+    )
+    spark.sql("drop table stg.emedia_sem_audience_mapping")
+    emedia_sem_audience_mapping_df.withColumn("etl_date", F.current_date()).distinct().write.mode(
+        "overwrite").saveAsTable("stg.emedia_sem_audience_mapping")
---
+++ emedia/processing/jd_new/tb_media_emedia_jdkc_daily.py
@@ -0,0 +1,60 @@
+#
+#
+#
+# def jdkc_daily_fact():
+#     # dwd.tb_media_emedia_jdkc_daily_fact
+#     # ds.hc_emedia_jdkc_deep_dive_download_adgroup_keyword_daily_fact
+#
+#
+#     # dwd.jdkc_keyword_daily
+#     # dwd.jdkc_adgroup_daily
+#
+#
+#     return 0
+#
+# 'ad_date', 'pin_name', 'effect', 'effect_days', 'mdm_productline_id', 'emedia_category_id', 'emedia_brand_id', 'mdm_category_id', 'mdm_brand_id', 'campaign_id', 'campaign_name', 'adgroup_id', 'adgroup_name', 'keyword_name', 'cost', 'clicks', 'impressions', 'cpc', 'cpm', 'ctr', 'total_order_roi', 'total_order_cvs', 'direct_cart_cnt', 'indirect_cart_cnt', 'total_cart_quantity', 'direct_order_value', 'indirect_order_value', 'order_value', 'direct_order_quantity', 'indirect_order_quantity', 'order_quantity', 'total_presale_order_cnt', 'total_presale_order_sum', 'business_type', 'gift_flag', 'order_status_category', 'click_or_order_caliber', 'start_day', 'end_day', 'is_daily', 'data_source', 'dw_batch_id', 'campaign_subtype', 'keyword_type', 'niname', 'dw_etl_date'
+#
+# ad_date
+# ad_format_lv2
+# pin_name
+# effect
+# effect_days
+# campaign_id
+# campaign_name
+# campaign_subtype
+# adgroup_id
+# adgroup_name
+# report_level
+# report_level_id
+# report_level_name
+# sku_id
+# keyword_type
+# niname
+# emedia_category_id
+# emedia_brand_id
+# mdm_category_id
+# mdm_brand_id
+# mdm_productline_id
+# delivery_version
+# delivery_type
+# mobile_type
+# source
+# business_type
+# gift_flag
+# order_status_category
+# click_or_order_caliber
+# put_type
+# campaign_put_type
+# cost
+# click
+# impression
+# order_quantity
+# order_value
+# total_cart_quantity
+# new_customer_quantity
+# dw_source
+# dw_create_time
+# dw_batch_number
+# etl_source_table
+# etl_create_time
+# etl_update_time
