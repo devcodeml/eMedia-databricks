fb50d2bf378622088dc84e6f99337c33ba2473ca
song-yw-1 <songyawei@mininglamp.com>
2022-11-23 17:34:58 +0800

    更新ppzq overview的cost计算

--- emedia/processing/overview/emedia_overview_daily_fact.py
+++ emedia/processing/overview/emedia_overview_daily_fact.py
@@ -98,7 +98,7 @@ def emedia_overview_etl():
         select ad_date,ad_format_lv2
         ,case when store_id in ('2017011','201701101','201701102','201701103') then 'tms' else 'tmall' end as platform
         ,store_id,effect,effect_days,emedia_category_id,emedia_brand_id,mdm_category_id,mdm_brand_id
-        , sum(cost) as cost
+        , sum(cost_new) as cost
         , sum(click) as click
         , sum(impression) as impression
         , 0 as uv_impression
@@ -108,7 +108,10 @@ def emedia_overview_etl():
         , 0 as gmv_quantity
         , 0 as new_customer_quantity 
         , 'dwd.tb_media_emedia_ppzq_daily_fact' as etl_source_table
-        from dwd.tb_media_emedia_ppzq_daily_fact where effect in ('1','3','15') and emedia_category_id is not null and emedia_category_id!='' group by ad_date,ad_format_lv2,store_id,effect,effect_days,emedia_category_id,emedia_brand_id,mdm_category_id,mdm_brand_id
+        from (select a.*,b.cost as cost_new from (select * from dwd.tb_media_emedia_ppzq_daily_fact where effect in ('1','3','15') and emedia_category_id is not null and emedia_category_id!='') a 
+        left join  (SELECT DISTINCT  brand_id, category_id, start_date, end_date, cost / (datediff(end_date,start_date) + 1) AS cost from stg.media_emedia_ppzq_cost_mapping where platform = 'ali') b
+        on a.emedia_category_id = b.category_id and a.emedia_brand_id = b.brand_id and a.ad_date>=b.start_date and a.ad_date<=b.end_date)
+        group by ad_date,ad_format_lv2,store_id,effect,effect_days,emedia_category_id,emedia_brand_id,mdm_category_id,mdm_brand_id
         union all
         select ad_date,ad_format_lv2
         ,case when store_id in ('2017011','201701101','201701102','201701103') then 'tms' else 'tmall' end as platform
@@ -222,7 +225,7 @@ def emedia_overview_etl():
         select ad_date,ad_format_lv2
         ,'jd' as platform
         ,'' as store_id,'' as effect,'' as effect_days,emedia_category_id,emedia_brand_id,mdm_category_id,mdm_brand_id
-        , sum(cost) as cost
+        , sum(cost_new) as cost
         , sum(click) as click
         , sum(impression) as impression
         , sum(uv_impressions) as uv_impression
@@ -232,7 +235,11 @@ def emedia_overview_etl():
         , 0 as gmv_quantity
         , 0 as new_customer_quantity
         , 'dwd.tb_media_emedia_jd_ppzq_daily_fact' as etl_source_table 
-        from dwd.tb_media_emedia_jd_ppzq_daily_fact where emedia_category_id is not null and emedia_category_id !='' group by ad_date,ad_format_lv2,emedia_category_id,emedia_brand_id,mdm_category_id,mdm_brand_id 
+        from (select a.*,b.cost as cost_new from (select * from dwd.tb_media_emedia_jd_ppzq_daily_fact where emedia_category_id is not null and emedia_category_id !='') a 
+        left join  (select DISTINCT q.emedia_brand_code as brand_id, category_id, start_date, end_date, cost / (datediff(end_date,start_date) + 1) AS cost from
+(SELECT * from stg.media_emedia_ppzq_cost_mapping where platform = 'jd') p left join (select distinct emedia_brand_code,brand_en from ods.media_category_brand_mapping where emedia_brand_code is not null) q on p.brand = q.brand_en) b
+        on a.emedia_category_id = b.category_id and a.emedia_brand_id = b.brand_id and a.ad_date>=b.start_date and a.ad_date<=b.end_date)
+        group by ad_date,ad_format_lv2,emedia_category_id,emedia_brand_id,mdm_category_id,mdm_brand_id 
     """)
 
     overview_df = condition_one.unionAll(condition_two).unionAll(condition_three)
